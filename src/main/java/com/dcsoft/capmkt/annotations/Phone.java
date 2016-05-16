f8e47d0a2b004d6798b04ba840b06a96a9cd05d7
Deep Chandra Kholia <dkholia@users.noreply.github.com>
2016-05-16 11:14:14 -0400

    Adding Phone annotation and PhoneValidator

---
+++ src/main/java/com/dcsoft/capmkt/annotations/Phone.java
@@ -0,0 +1,26 @@
+package com.dcsoft.capmkt.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+import com.dcsoft.capmkt.bo.transferobj.validators.PhoneValidator;
+
+@Documented
+@Constraint(validatedBy = PhoneValidator.class)
+@Target( { ElementType.METHOD, ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Phone {
+ 
+    String message() default "{Phone}";
+     
+    Class<?>[] groups() default {};
+     
+    Class<? extends Payload>[] payload() default {};
+      
+}
