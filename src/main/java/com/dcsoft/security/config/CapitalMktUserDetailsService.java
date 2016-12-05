8346a16b85a62ef31bd38b2d4b9553979af1f8ef
Deep Chandra Kholia <dkholia@users.noreply.github.com>
2016-12-04 23:55:12 -0500

    Signed-off-by: Deep Chandra Kholia <dkholia@users.noreply.github.com>

--- src/main/java/com/dcsoft/security/config/CapitalMktUserDetailsService.java
+++ src/main/java/com/dcsoft/security/config/CapitalMktUserDetailsService.java
@@ -33,8 +33,6 @@ public class CapitalMktUserDetailsService implements UserDetailsService {
 		
 	}
 
-	// Converts com.mkyong.users.model.User user to
-	// org.springframework.security.core.userdetails.User
 	private User buildUserForAuthentication(com.dcsoft.security.config.Users user, List<GrantedAuthority> authorities) {
 		return new User(user.getUsername(), user.getPassword(), user.getEnabled(), true, true, true, authorities);
 	}
